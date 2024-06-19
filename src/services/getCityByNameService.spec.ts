d23791cbbece94a06bffbad35eca018dbddaf3b7
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-06-19 17:03:27 -0300

    Mock folder

--- src/services/getCityByNameService.spec.ts
+++ src/services/getCityByNameService.spec.ts
@@ -1,21 +1,10 @@
+import { mockCityApiResponse } from "_tests/mockCityApiResponse";
 import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 
 describe("api: getCityByNameService", () => {
-  it('should return city information',async () => {
-    const data = {
-      id:'1',
-      name:'São Paulo',
-      sys:{
-        country:'BR'
-      },
-      coord:{
-        lat:123,
-        lon:456
-      }
-    };
-    
-    jest.spyOn(api,'get').mockResolvedValue({data});
+  it('should return city information',async () => {  
+    jest.spyOn(api,'get').mockResolvedValue({data : mockCityApiResponse});
     const response = await getCityByNameService("São Paulo")
     expect(response.length).toBeGreaterThan(0)
   })
