8733837d06d97795fa2a90085a04e42816dcdaaf
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-06-30 18:15:00 -0300

    Finishing the Dashboard test

--- src/screens/Dashboard/dashboard.spec.tsx
+++ src/screens/Dashboard/dashboard.spec.tsx
@@ -2,8 +2,10 @@
 import { api } from "@services/api"
 
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
+
+import { mockCityApiResponse } from "../../../_tests/mock/api/mockCityApiResponse"
 import { mockWeatherAPIResponse } from "../../../_tests/mock/api/mockWeatherApiResponse"
-import { render, screen, waitFor } from "../../../_tests/utils/customRender"
+import { act, fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "../../../_tests/utils/customRender"
 import { Dashboard } from "./index"
 
 describe("Screen : Dashboard",() => {
@@ -21,5 +23,35 @@ describe("Screen : Dashboard",() => {
     const cityName = await  waitFor(() => screen.findByText(/rio do sul/i))
 
     expect(cityName).toBeTruthy()
+  })
+
+  it("should be show another selected  weather city", async () => {
+
+
+    const city = {
+      id:'1',name:'rio do sul ', latitude:123, longitude:456
+    }
+
+    await saveStorageCity(city)
+
+    jest.spyOn(api, 'get').mockResolvedValueOnce({data: mockWeatherAPIResponse})
+    .mockResolvedValueOnce({data: mockCityApiResponse})
+    .mockResolvedValueOnce({data: mockWeatherAPIResponse})
+
+    render(<Dashboard/>)
+
+    await waitForElementToBeRemoved(() =>  screen.queryByTestId('loading'))
+
+    const cityName = 'São Paulo' 
+    await  waitFor(() => act(() => {
+      const search = screen.getByTestId('search-input')
+      fireEvent.changeText(search, cityName)
+    }))
+
+    await waitFor(() => act(() => {
+      fireEvent.press(screen.getByText(cityName, {exact:false}))
+    }))
+
+    expect(screen.getByText(cityName, {exact: false})).toBeTruthy()
   })
 })
